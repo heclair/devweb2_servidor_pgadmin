d8a352c203667e8910c7c5b994f4f630ae41a235
heclair <heclair@hotmail.com>
2023-09-04 21:01:47 -0300

    Express com PGSQL AULA 04 04/09

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,16 @@
 import {Request, Response} from "express";
+import {User} from "../entities/User";
+import DataSource from "../data-source";
 
 class UserController{
-    create(req:Request, res:Response){
+    async create(req:Request, res:Response){
         const {mail,password}=req.body;
+
+        const user = new User();
+        user.mail = mail;
+        user.password = password;
+        const r = await DataSource.manager.save(User, user);
+        res.json(r);
     }
 
 }
